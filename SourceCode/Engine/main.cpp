d8b7a9552890cc13894ea56cf191a85eb93c5397
Peter Merkert <peter.merkert@gmx.de>
2016-01-26 17:03:56 +0000

    Setup default project with SDL2. Small bug still remaining. Fix in the next commit

--- SourceCode/Engine/main.cpp
+++ SourceCode/Engine/main.cpp
@@ -1,10 +1,24 @@
-#include <iostream>
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "SDL2main.lib")
 
-#pragma comment(lib, "../lib/SDL2.dll")
+#include <iostream>
+#include "SDL.h"
 
 int main() {
+	
+	if ((SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1)) {
+		printf("Could not initialize SDL: %s.\n", SDL_GetError());
+		exit(-1);
+	}
+
+	
 	std::cout << "hello" << std::endl;
 
+
+
 	int x; std::cin >> x;
+
+	SDL_Quit();
+
 	return 0;
 }
